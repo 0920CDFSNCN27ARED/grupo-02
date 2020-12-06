c0e50c2b60ff4f7b93266dbf4c04cb8b9a1ac09a
ilianaJofre <ilianamicaela@gmail.com>
2020-12-06 01:05:40 -0300

    Creamos la carpeta routes y modificamos app.js. Renombramos los archivos .html a .ejs

--- Site/myapp/app.js
+++ Site/myapp/app.js
@@ -1,29 +1,19 @@
 const express = require('express');
 const path = require('path');
+const indexRoutes = require('./routes/index');
+const userRoutes = require('./routes/users');
+const cartRoutes = require('./routes/cart');
+const productRoutes = require('./routes/product');
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
 });
 
-app.get('/cart', (req,res)=>{
-    res.sendFile(__dirname + '/views/cart.html');
-});
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
-});
-
-app.get('/producDetail', (req,res)=>{
-    res.sendFile(__dirname + '/views/producDetail.html');
-});
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.use('/', indexRoutes);
+app.use('/user', userRoutes);
+app.use('/cart', cartRoutes);
+app.use('/product', productRoutes);
