2d04d9a4abe9e14a3f21ea12eddb05faf4d54aa3
ilianaJofre <ilianamicaela@gmail.com>
2020-12-07 17:48:40 -0300

    Se agrega .ejs de productos

--- Site/myapp/controllers/productDetailController.js
+++ Site/myapp/controllers/productDetailController.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 let products = JSON.parse(fs.readFileSync(__dirname + '/../data/products.json', {encoding: 'utf-8'}));
 
-let productController = {
+let productDetailController = {
     getAll:()=>{
         return products;
     },
     getProduct: (req,res)=>{
-        res.render('product');
+        res.render('productDetail');
     },
 };
 
-module.exports = productController;
+module.exports = productDetailController;
--- Site/myapp/routes/product.js
+++ Site/myapp/routes/product.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const productDetailController = require('../controllers/productDetailController');
 const productController = require('../controllers/productController');
+
+router.get('/detail', productDetailController.getProduct);
 
 router.get('/', productController.getProduct);
 
