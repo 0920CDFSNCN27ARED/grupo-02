92b4054828bf3118228b4d0fc6195b2ec0d3e364
Enzo Norberto Becce <becce.enzo.norberto@gmail.com>
2020-12-12 13:51:40 -0300

    Agregamos dinamismo a productDetail y modificamos la vista.

--- Site/myapp/controllers/productController.js
+++ Site/myapp/controllers/productController.js
@@ -4,6 +4,17 @@ let productController = {
         const products = getProduct();
         res.render('product', {products: products});
     },
+    getProductDetail: (req, res)=>{
+        const products = getProduct();
+        const product = products.find((prod)=>{
+            return prod.id == req.params.id
+        });
+        if(!product){
+            res.send('Error <404> No se encontró el producto solicitado')
+        }else{
+            res.render('productDetail',{products: product})
+        }
+    },
 };
 
 module.exports = productController;
--- Site/myapp/routes/product.js
+++ Site/myapp/routes/product.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const productDetailController = require('../controllers/productDetailController');
 const productController = require('../controllers/productController');
 
 router.get('/', productController.getProduct);
-router.get('/:id/detail', productDetailController.getProduct);
-
-router.get('/:id/edit', productDetailController.editProductForm);
-/*router.put('/:id/edit', productDetailController.editProduct);
+router.get('/:id/detail', productController.getProductDetail);
+/*
+router.get('/:id/edit', productController.editProductForm);
+router.put('/:id/edit', productDetailController.editProduct);
 
 router.get('/newProduct', productDetailController.newProductForm);
 router.post('/newProduct', productDetailController.newProduct);
