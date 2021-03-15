341002486ee728d036fc2cd14a1a1ca5611ca461
ilianaJofre <ilianamicaela@gmail.com>
2021-03-15 12:17:37 -0300

    validación Login Front JS

---
+++ Site/myapp/public/js/validation-login.js
@@ -0,0 +1,71 @@
+let errors = [];
+window.addEventListener('load', function() {
+    let formulario = document.querySelector('form.login');
+    console.log('soy login');
+    formulario.onsubmit = function (event)  {
+        errors = [];
+        clearValidations();
+        const msgLength = "El campo no puede estar vacío";
+        
+        validateLength('password', 'passwordFeedback', 'La contraseña debe tener al menos 8 caracteres', 8);
+
+        validateNotEmpty('email', 'emailFeedback', msgLength);
+        validateNotEmpty('password', 'passwordFeedback', msgLength);
+
+        if (checkErrors()){
+            event.preventDefault();
+        }
+    }
+});
+function validateNotEmpty (input, feedbackID, errorMsg) {
+    const inputValid = document.getElementById(input);
+    if (inputValid.value.trim() == '') {
+        const error = {
+            feedbackID,
+            input,
+            errorMsg,
+        };
+        errors.push(error);
+    } else {
+        inputValid.classList.add('is-valid');
+    }
+};
+function validateLength (input, feedbackID, errorMsg, minLength) {
+    const inputValid = document.getElementById(input);
+    if (inputValid.value.length < minLength) {
+        const error = {
+            feedbackID,
+            input,
+            errorMsg,
+        };
+        errors.push(error);
+    } else {
+        inputValid.classList.add('is-valid');
+    }
+};
+function clearValidations() {
+    const overallInputs = document.getElementsByClassName('overall');
+    const feedbackmsg = document.getElementsByClassName('feedback');
+  
+    for (let input of overallInputs) {
+      input.classList.remove('is-invalid');
+    }
+    for (let feedback of feedbackmsg) {
+      feedback.classList.remove('alert', 'alert-danger');
+      feedback.innerHTML = '';
+    }
+}
+function checkErrors(){
+    if (errors.length > 0){
+        console.log(errors);
+        errors.forEach((e) => {
+            const feedbackDiv = document.getElementById(e.feedbackID);
+            const input = document.getElementById(e.input);
+            feedbackDiv.innerHTML = e.errorMsg;
+            input.classList.add('is-invalid');
+            feedbackDiv.classList.add('alert', 'alert-danger')
+        }); 
+        return true;
+    }
+    return false;
+};
