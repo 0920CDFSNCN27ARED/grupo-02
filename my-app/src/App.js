06bac30bac3f256aa4cedb9f13c431f562c3a6be
ilianaJofre <ilianamicaela@gmail.com>
2021-04-01 18:04:04 -0300

    Se sube componentes del dashboard

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import SideMenu from './components/side-menu/side-menu';
 import Header from './components/header/header';
 import DataCardSmall from './components/data-cards/data-card-small/data-small-card';
 import DataCardBig from "./components/data-cards/data-card-big/data-big-card";
+import DataBigCardCategory from "./components/category-card/data-big-card-category"; 
+import ProductDetailCard from "./components/product-detail-card/ProductDetail"
 import './App.css';
 
 function App() {
@@ -9,18 +11,26 @@ function App() {
     <div className='App'>
       <div id='wrapper'>
         <SideMenu/>
-        <div id='content-wrapper' class='d-flex flex-column'>
+        <div id='content-wrapper' className='d-flex flex-column'>
           <div id='content'>
             <Header/>
           </div>
-          <div class="container-fluid">
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-              <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
+          <div className="container-fluid">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
             </div>
-            <div class="row">
+            <div className="row">
               <DataCardSmall title= 'Products in Data Base' value='13' icon='fa-clipboard-list' color= 'primary'/>
               <DataCardSmall title= 'Amount in products' value='$ 23000' icon='fa-dollar-sign' color= 'success'/>
               <DataCardSmall title= 'Users quantity' value='20' icon='fa-user-check' color= 'warning'/>
+            </div>
+            <div className="row">
+                <DataCardBig />
+                <DataBigCardCategory />
+            </div>
+            <h1 className="h3 mb-2 text-gray-800">All the products in the Database</h1>
+            <div className="card shadow mb-4">
+              <ProductDetailCard />
             </div>
           </div>
         </div>
